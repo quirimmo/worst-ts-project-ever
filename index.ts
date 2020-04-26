82ccec7899681e97b58200cedc66be172803f10c
lgrden <lgrden@gmail.com>
2020-04-26 18:35:50 +0200

    some test

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
 import { Fibonacci } from './math/fibonacci';
+import { User } from './model/user';
 
 console.log(Fibonacci.calculate(15));
+
+const me = new User('Lukasz', 'Master', 38);
+
+console.log(me);
+
+interface A{}
+
+class Teacher extends User implements A {
+  teach(): void {
+    console.log('Welcome to class! My name is ' + this.name);
+  }
+}
+
+const teacher = new Teacher('remo', 'jansen', 22);
+teacher.teach();
+
+console.log(teacher instanceof User);
